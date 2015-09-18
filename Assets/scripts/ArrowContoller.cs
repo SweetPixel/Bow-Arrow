059222675fe3d71705012f2456db49d3e30ea2ff
zahid--ali <zfz.zahid@yahoo.com>
2015-09-18 11:48:52 +0500

    Initial Commit

---
+++ Assets/scripts/ArrowContoller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ArrowContoller : MonoBehaviour {
+	Touch t;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	public GameObject bow;
+	public Text text;
+
+	void Update(){
+		if (Input.touchCount > 1) {
+			t = Input.touches [1];
+			if (t.phase == TouchPhase.Began) {
+				text.text = t.position.x.ToString();
+				screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+				offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (t.position.x, t.position.y, screenPoint.z));
+			}
+			
+			if (t.phase == TouchPhase.Moved) {
+				text.text = t.position.x.ToString();
+				Vector3 curScreenPoint = new Vector3 (t.position.x, t.position.y, screenPoint.z);
+				Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint), offset;
+				bow.transform.position = curPosition;
+			}
+
+		}
+	}
+}
---
+++ Assets/scripts/Mousedrag.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Mousedrag : MonoBehaviour 
+{
+	public Text test;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	Touch t;
+
+	void Update(){
+		if (Input.touchCount > 0) {
+			t = Input.touches[0];
+			//foreach(Touch t in Input.touches){
+				if (t.phase == TouchPhase.Began){
+
+					screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+					offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(t.position.x, t.position.y, screenPoint.z));
+				}
+
+				if(t.phase == TouchPhase.Moved){
+
+					Vector3 curScreenPoint = new Vector3(t.position.x, t.position.y, screenPoint.z);
+					Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) , offset;
+					transform.position = curPosition;
+				}
+			//}
+		}
+	}
+
+
+	/*void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		
+	}*/
+	
+	/*void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) , offset;
+		transform.position = curPosition;
+		
+	}*/
+	
+}

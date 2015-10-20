a51f9d5754b79fee6eed34a85297469b4db1b5bf
ebtihaj-khan <ebtihaj_316@yahoo.com>
2015-10-20 12:45:50 +0500

    arrow instantiate

--- Assets/scripts/RopeMidTest.cs
+++ Assets/scripts/RopeMidTest.cs
@@ -6,41 +6,39 @@ public class RopeMidTest : MonoBehaviour {
 		Touch t;
 		private Vector3 screenPoint;
 		private Vector3 offset;
+		public Sprite onlyBow;
+		public Sprite BowandArrow;
 
-		public LineRenderer top;
-		public LineRenderer bottom;
-
-	//	GameObject mid;
 		
-		void Start()
-		{
-			this.gameObject.GetComponent<MeshRenderer>().enabled=false;
-		}
-		
-		void Update(){
-			if (Input.touchCount > 1) 
-		{
-				t = Input.touches [1];
-				if (t.phase == TouchPhase.Began)
+		void Update()
+			{
+					if (Input.touchCount > 1) 
 				{
-					GameObject.Find("BowTopPoint").GetComponent<LineRenderer>();
-					this.gameObject.GetComponent<MeshRenderer>().enabled=true;
-					screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-					offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(t.position.x, t.position.y, screenPoint.z));
+						t = Input.touches [1];
+						if (t.phase == TouchPhase.Began)
+						{
+							screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+							offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(t.position.x, t.position.y, screenPoint.z));
+						}
+						
+						if (t.phase == TouchPhase.Moved) 
+						{
+							GameObject.Find("BowandArrow").GetComponent<SpriteRenderer>().sprite=onlyBow;
+							GameObject.Find("Still Arrow").GetComponent<MeshRenderer>().enabled=true;
+							Vector3 curScreenPoint = new Vector3(t.position.x, t.position.y, screenPoint.z);
+							Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) , offset;
+							transform.position = curPosition;
+						}
+						if (t.phase == TouchPhase.Ended) 
+						{
+							GameObject.Find("BowandArrow").GetComponent<SpriteRenderer>().sprite=BowandArrow;
+							GameObject.Find("Still Arrow").GetComponent<MeshRenderer>().enabled=false;
+							resetString();
+						}
 				}
-				
-				if (t.phase == TouchPhase.Moved) 
-				{
-					Vector3 curScreenPoint = new Vector3(t.position.x, t.position.y, screenPoint.z);
-					Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) , offset;
-					transform.position = curPosition;
-				}
-				
-				if (t.phase == TouchPhase.Ended) 
-				{
-					this.gameObject.GetComponent<MeshRenderer>().enabled=false;
-				}
-
 			}
+	void resetString()
+		{
+		this.gameObject.transform.position = new Vector3 (0,0,0);
 		}
-	}
+}
